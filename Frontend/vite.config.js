eab0e1303104cae47bb3dad63e90ae860e48b21d
sameenith <mohammedsameenith@gmail.com>
2025-10-23 21:25:56 +0530

    changed localhost to new link

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -14,7 +15,7 @@ export default defineConfig({
     proxy: {
       // String shorthand: '/api' -> 'http://localhost:8000/api'
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.URL,
         changeOrigin: true,
       },
     },
